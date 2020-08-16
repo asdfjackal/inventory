3f03582f1b58983d6f134eeee311af20c14c32ed
Aleksandr White <aleksandr@asdfjackal.com>
2020-08-15 23:06:02 -0400

    Updates

---
+++ precache-manifest.d6c023986b3b7645b1bc4d6e22480aae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4670777ddc37b84f98d33d67cf47d69",
+    "url": "/inventory/index.html"
+  },
+  {
+    "revision": "57d10b0d0976c2f66c77",
+    "url": "/inventory/static/js/2.b345554c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/inventory/static/js/2.b345554c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d03b021e484b58f2a760",
+    "url": "/inventory/static/js/main.148c9714.chunk.js"
+  },
+  {
+    "revision": "06ac5b5243d9f1d0416e",
+    "url": "/inventory/static/js/runtime-main.be693f39.js"
+  }
+]);
